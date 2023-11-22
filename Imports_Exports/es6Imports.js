b43cf4b135193d27202d1192ca87978a236b296e
Francisco Campos <sasasasamaes13@gmail.com>
2023-11-21 21:26:29 -0600

    export dafault

--- Imports_Exports/es6Imports.js
+++ Imports_Exports/es6Imports.js
@@ -1,8 +1,17 @@
+// export const funcionA = () => {
+//     console.log("funcionA")
+// }
+// export const functionB = () => {
+//     console.log("funcionB")
+// }
+
 const funcionA = () => {
-    console.log("funcionA")
+  console.log("funcionA que tambien default")
 }
-export const functionB = () => {
-    console.log("funcionB")
+const functionB = () => {
+  console.log("funcionB")
 }
+
+export { funcionA, functionB }
 // Exportar ES6
 export default funcionA
--- Imports_Exports/index.js
+++ Imports_Exports/index.js
@@ -1,9 +1,12 @@
 // CommonJs
 // let {funcionA,funcionB } = require('./commonJs.js')
 // es6 imports
-import funcionA,{ functionB }  from './es6Imports.js'
-
+// import funcionA,{ functionB }  from './es6Imports.js'
+import { funcionA,functionB }  from './es6Imports.js'
+import nombre from './es6Imports.js'
 // a.variableAexportar()
 // a.variableAexportarB()
 funcionA()
 functionB()
+
+nombre()
