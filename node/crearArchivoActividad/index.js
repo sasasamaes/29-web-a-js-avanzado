e6b3c592e8d5819e9f3d56f4d2f11ab355a1bee2
Francisco Campos <sasasasamaes13@gmail.com>
2023-11-23 19:37:41 -0600

    crear archivo

---
+++ node/crearArchivoActividad/index.js
@@ -0,0 +1,16 @@
+// Crear un pequeno script que cree un archivo con el nombre de la actividad que le indique
+
+const fs = require('fs')
+
+const path = './'
+const fileName = 'archivoActividad.txt'
+const contentFile = 'Archivo creado por el script'
+const destination = `${path}${fileName}`
+
+fs.writeFile(destination,contentFile, (err) => {
+  if(err) {
+    throw err;
+  } else {
+    console.log(`El archivo ${fileName} se ha creado correctamente`)
+  }
+})
